9b1ed65f744e7c4dd7e49963495a0382f258231f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-18 18:32:04 +0530

    [Tejas] add Triple sum zero

--- let numArray = new Array();.js
+++ let numArray = new Array();.js
@@ -1,15 +1,24 @@
-
-
-const prompt = require('prompt-sync')();
-
-let num = prompt("Enter a Number:  ")
-num = parseInt(num)
-let factors = new Array()
-for(let factor = 2; factor <= num; factor++){
-    while(num % factor == 0){
-        factors.push(factor);
-        num = num / factor;
+function findTriplets(arr, n) {
+    arr.sort();
+    var l = arr.length;
+    for (var i = 0; i < l; i++) {
+        var j = i + 1,
+            k = l - 1;
+        while (j < k) {
+            if (arr[i] + arr[j] + arr[k] < n) {
+                j++;
+            } else if (arr[i] + arr[j] + arr[k] > n) {
+                k--;
+            } else {
+                console.log(arr[i] + "," + arr[j] + "," + arr[k]);
+                j++;
+                k--;
+            }
+        }
     }
+    return true;
 }
 
-console.log("Prime Factorization of " + num + " is:  " + factors.toString())
+var arr = [-1, -4, -9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+findTriplets(arr, 0);
+
